dd7d98f2fe72637c7eb7976953d37f618bb649e8
sota1231 <bacchuszzzzzzyyyyyy@gmail.com>
2025-04-04 12:13:07 +0900

    短文フォルダ追加・サイドバーにアドバイス追加

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,49 +20,66 @@ const Sidebar = ({
   return (
     <div className='app-sidebar'>
       <div className='sidebar_start_position'>
-        <span>ようこそ、{userName}さん！</span>
+        <span>ログインユーザー：{userName}</span>
         <button className='center' onClick={handleLogout}>ログアウト</button>
       </div>
       <div className='app-sidebar-header'>
 
         <div className='app-sidebar-nav'>
           <div className='app-wordlist-not'>
-                <Link
-                  to="/words/1"
-                  className={wordId === '1' ? 'active' : ''}
-                >
-                  日常会話　1
-                </Link>
+            <Link
+              to="/words/1"
+              className={wordId === '1' ? 'active' : ''}
+            >
+              日常会話　1
+            </Link>
 
-                <Link
-                  to="/words/2"
-                  className={wordId === '2' ? 'active' : ''}
-                >
-                  日常会話　2
-                </Link>
+            <Link
+              to="/words/2"
+              className={wordId === '2' ? 'active' : ''}
+            >
+              日常会話　2
+            </Link>
 
-                <Link
-                  to="/words/3"
-                  className={wordId === '3' ? 'active' : ''}
-                >
-                  旅行先
-                </Link>
+            <Link
+              to="/words/3"
+              className={wordId === '3' ? 'active' : ''}
+            >
+              旅行先
+            </Link>
 
-                <Link
-                  to="/words/4"
-                  className={wordId === '4' ? 'active' : ''}
-                >
-                  映画
-                </Link>
+            <Link
+              to="/words/4"
+              className={wordId === '4' ? 'active' : ''}
+            >
+              映画
+            </Link>
 
-                <Link
-                  to="/words/5"
-                  className={wordId === '5' ? 'active' : ''}
-                >
-                  その他
-                </Link>
+            <Link
+              to="/words/5"
+              className={wordId === '5' ? 'active' : ''}
+            >
+              単語・短文
+            </Link>
+
+            <Link
+              to="/words/6"
+              className={wordId === '6' ? 'active' : ''}
+            >
+              その他
+            </Link>
 
           </div>
+        </div>
+        <div className='advice'>
+          <span>聞き取れない原因は２つ</span><br></br>
+          ①知らない単語・短文　→　短文を覚える<br></br>
+          ②正しい発音を知らない　→　リンキング、シャドーイング<br></br>
+          <br></br>
+          またリスニングには<br></br>
+          音声知覚（聴こえた音）<br></br>
+          意味理解（文を塊で理解,声色,話の流れ）<br></br>
+          がある。<br></br>
         </div>
       </div>
     </div>
